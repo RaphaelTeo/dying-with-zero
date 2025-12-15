300b7a71391b9a386d6b95d4ea28d6351eed01cb
Raphael Teo <78261352+RaphaelTeo@users.noreply.github.com>
2025-12-15 22:10:34 +0800

    restructure for pythonanywhere

--- mysite/app.py
+++ mysite/app.py
@@ -1,57 +1,51 @@
+
 from flask import Flask, request, render_template
-from flask_cors import CORS
 import pandas as pd
 pd.set_option('display.float_format', '{:.0f}'.format)
-import matplotlib
-matplotlib.use('Agg')  # Force non-interactive backend
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-#import os
 import io
 import base64
-import sys
-import traceback
 
 app = Flask(__name__)
-CORS(app) # Enable CORS for all routes
 
 def create_df(age,retirement_age,final_age,age_lst,expenses_lst,income,init,returns):
-        income_lst = [income] * (retirement_age-age-1)
-        zeroincome = [0] * (final_age-retirement_age)
-        income_lst.insert(0,0)
-        income_lst.extend(zeroincome)
+    income_lst = [income] * (retirement_age-age-1)
+    zeroincome = [0] * (final_age-retirement_age)
+    income_lst.insert(0,0)
+    income_lst.extend(zeroincome)
 
-        df = pd.DataFrame(columns=['age','income','expenses','cumulative'])
-        df['age']=age_lst
-        df['expenses']=expenses_lst
-        df['income']=income_lst
-        cum_calc = []
-        for i in range(len(df)):
-            if i == 0:
-                cum_calc.append(init)
-            else:
-                cum_calc.append((cum_calc[i-1]*returns) + df.loc[i,'income'] - df.loc[i, 'expenses'])
+    df = pd.DataFrame(columns=['age','income','expenses','cumulative'])
+    df['age']=age_lst
+    df['expenses']=expenses_lst
+    df['income']=income_lst
+    cum_calc = []
+    for i in range(len(df)):
+        if i == 0:
+            cum_calc.append(init)
+        else:
+            cum_calc.append((cum_calc[i-1]*returns) + df.loc[i,'income'] - df.loc[i, 'expenses'])
 
-        df['cumulative'] = cum_calc
-        return df
+    df['cumulative'] = cum_calc
+    return df
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     error = None
     image_data = None
+    summary = None
     if request.method == 'POST':
         try:
             age = int(request.form['age'])
             retage = int(request.form['retage'])
-            returns = float(request.form['returns'])
-            returns = (returns/100)+1
+            returns_perc = round(float(request.form['returns']),2)
+            returns = round((returns_perc/100)+1,5)
             income = float(request.form['income'])
             expenses = float(request.form['expenses'])
             init = float(request.form['init'])
 
             final_age = 121
             retirement_age_list = list(range(age+1,retage+1,1))
-            #retirement_age_list = [retage]
             age_lst = [i for i in  range(age,final_age)]
             expenses_lst = [expenses] * (final_age-age-1)
             expenses_lst.insert(0,0)
@@ -66,29 +60,29 @@ def index():
             plt.xlabel('Age')
             plt.xticks(rotation=45)
             plt.ylabel('Cumulative $ (\'000)')
-            #plt.title(f'Sustainability at different retirement ages at {round((returns-1),2)*100}% return')
+            plt.title(f'DWZ plan for different retirement ages at {returns_perc}% return')
             plt.tight_layout()
             plt.legend(bbox_to_anchor=(0, 1), loc='upper left')
             plt.ylim(bottom=-200,top=2000)
             ax = plt.gca() # get current axis
-            ax.xaxis.set_major_locator(ticker.MultipleLocator(5))  
+            ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
             ax.yaxis.set_major_locator(ticker.MultipleLocator(200))
             #plt.savefig(f'Sustainability {init_cumulative}k inc {yearly_income}k exp {yearly_expense}k at {round((returnedcapital-1),2)*100} percent.png', dpi=300)
             #plt.show()
 
             # Output plot directly to BytesIO
             buf = io.BytesIO()
-            plt.savefig(buf, format='png', bbox_inches='tight')
+            plt.savefig(buf, format='png', bbox_inches='tight', dpi=100)
             buf.seek(0)
             image_data = base64.b64encode(buf.getvalue()).decode('utf-8')
             plt.close()
 
-        except Exception as e:
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            trace = [i for i in traceback.format_tb(exc_traceback)]
-            error = f"{str(e)} | {exc_type.__name__} | {exc_value} | {trace}"
+            summary = f'DWZ plan with {init}k + income {income}k + expenses {expenses}k at {returns_perc} percent'
 
-    return render_template("index.html", error=error, image_data=image_data)
+        except Exception as e:
+            error = f"error: {e}"
+
+    return render_template("index.html", error=error, image_data=image_data, summary=summary)
 
 if __name__ == '__main__':
     app.run(debug=True)
